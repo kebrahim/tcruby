841b7d6cd615d69e9b76f184fa95cfe6bee389a4
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2013-10-03 16:58:40 -0400

    create rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tcruby::Application.config.secret_token = '3a23580a86e03c703e2e848b5811b6045588fe5f43242a7e63e7f1a765ede455c1e3635f8b789aba0d1f6c458e125217e51425b1ae851c43117fadcbd4f61032'
